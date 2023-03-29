b282e0ec5749f22b107694e9a15598fbd930cb5b
NiranjanNani <70282898+NiranjanNani@users.noreply.github.com>
2023-03-29 13:58:00 +0530

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -142,12 +142,6 @@ MESSAGE_TAGS={
   messages.ERROR :'danger'
 }
 
-# Email Config
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT=587
-EMAIL_HOST_USER=''
-EMAIL_HOST_PASSWORD=''
-EMAIL_USE_TLS=True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
